9fcdaf64b74ceb3e316a9dadbe4ec475e05d8ab7
Daniel Nguyen <danguyen@spawar.navy.mil>
2022-06-22 13:47:04 -0700

    add pose service

--- angular/app/src/app/shared/services/index.ts
+++ angular/app/src/app/shared/services/index.ts
@@ -7,3 +7,4 @@ export { TesterApiService } from './tester/tester-api.service';
 export { CampaignApiService } from './campaign/campaign-api.service';
 export { ThemeService } from './theme/theme.service';
 export { EntityApiService } from './entity/entity-api.service';
+export { PoseApiService } from './map/pose-api.service';
---
+++ angular/app/src/app/shared/services/map/pose-api.service.ts
@@ -0,0 +1,126 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import {Subscription, Subject, interval, Observable, BehaviorSubject, ReplaySubject} from 'rxjs';
+import { map, expand } from "rxjs/operators";
+
+import { TrialApiService } from '../trial/trial-api.service';
+import { PosePageResult } from '../../models/map.model';
+import { Pose } from "../../models";
+import { PoseAdapters } from './pose.adapter'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PoseApiService implements OnDestroy {
+  private subscriptions = new Subscription();
+  
+  private posesSubject: Subject<Pose[]>;
+  private posesByPoseSourceSubject: Subject<Object>;
+
+
+  private selectedTrialId: number;
+  private INTERVAL_TIME: number = 2000; // 2 second
+
+  constructor(private http: HttpClient, private _trialApiService: TrialApiService) { 
+    this.posesSubject = new Subject();
+    this.posesByPoseSourceSubject = new Subject();
+
+
+    // Subscribe to the Trial
+    // Whenever the trial changes, this.getInitialEvents will be called
+    this.subscriptions.add(this._trialApiService.getSelectedTrial().subscribe(
+      data=>{
+        if (!data) {
+          return;
+        }
+
+        // trial-api service will detect the changes
+        if(data.id) {
+          this.selectedTrialId = data.id;
+          
+          // some way to retrieve initial poses
+          this.getPosesInitial();
+
+        }
+      },
+      err => console.error(err),
+    ));
+
+    this.subscriptions.add(interval(this.INTERVAL_TIME).subscribe(
+      x =>
+        {
+          console.log("nothing here");
+          // some way to refresh poses
+        }
+      ));
+  }
+
+  private getPosesInitial(): void {
+    let posesRequest: Observable<PosePageResult> = this.performSearch('/api/poses/', this.selectedTrialId)
+    this.subscriptions.add(posesRequest.subscribe(
+      (page: PosePageResult) => {
+        let main_object = Object();
+        page.results.forEach((pose) => {
+          if (pose.pose_source && pose.pose_source.name) {
+            if (!main_object.hasOwnProperty(pose.pose_source.name)) {
+              main_object[pose.pose_source.name] = [];
+            }
+            main_object[pose.pose_source.name] = [...main_object[pose.pose_source.name], pose];
+          }
+        })
+        this.posesByPoseSourceSubject.next(main_object);
+        console.log(main_object);
+
+        this.posesSubject.next(page.results);
+        if(page.next){
+          this.testing(page.next);
+        }
+
+      }
+    ));
+  }
+
+  testing(url: string) {
+    let posesRequest: Observable<PosePageResult> = this.performSearch(url)
+
+    this.subscriptions.add(posesRequest.subscribe(
+      (page: PosePageResult) => {
+        let main_object = Object();
+        page.results.forEach((pose) => {
+          if (pose.pose_source && pose.pose_source.name) {
+            if (!main_object.hasOwnProperty(pose.pose_source.name)) {
+              main_object[pose.pose_source.name] = [];
+            }
+            main_object[pose.pose_source.name] = [...main_object[pose.pose_source.name], pose];
+          }
+        })
+        this.posesByPoseSourceSubject.next(main_object);
+        console.log(main_object);
+
+        this.posesSubject.next(page.results);
+        if(page.next){
+          this.testing(page.next);
+        }
+      }
+    ));
+  }
+
+  getPoses(): Observable<Pose[]>{
+    return this.posesSubject.asObservable();
+  }
+
+  // break this out to clean up functions
+  performSearch = (url, some_id?) => {
+    const options = { 
+      params: some_id ? new HttpParams().set('trial', some_id) : {}
+    };
+    return this.http.get(url, options).pipe(
+      map((data: any) => PoseAdapters.posePageResultAdapter(data) ),
+    );
+  }
+
+  // clean up
+  ngOnDestroy(){
+    this.subscriptions.unsubscribe();
+  }
+}
